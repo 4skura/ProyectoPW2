08e70e636a727bac7693175cd5bf35e4acf5626e
4skura <askurat.t@gmail.com>
2022-11-30 00:54:48 -0600

    upload of the prject

---
+++ app6.js
@@ -0,0 +1,23 @@
+//funciones basicas de js
+function despedir(){
+    alert("bais desde una funcion en el body");
+}
+
+function bailar(){
+    alert("a poco sabes bailar??");
+}
+
+function comer(){
+    alert("ya hace hambre unu");
+}
+
+//consola
+console.log("Hola desde la consola");
+
+console.log("Este es un texto muy X");
+console.log(14);
+console.log(14.14);
+console.log(1,63);
+console.log(1+13);
+console.log(true);
+console.log(false);
